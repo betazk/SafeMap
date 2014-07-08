f4be88b2872d3c849b94f818c950bf72a2f0daa6
Gogs <gogitservice@gmail.com>
2014-07-08 15:46:04 +0800

    add the SafeMap.go file

---
+++ SafeMap.go
@@ -0,0 +1,102 @@
+package main
+
+import (
+	"fmt"
+)
+
+const (
+	remove commandAction = iota
+	end
+	find
+	insert
+	length
+	update
+)
+
+type SafeMap interface {
+	Insert(string, interface{})
+	Delete(string)
+	Find(string) (interface{}, bool)
+	Len() int
+	Update(string, UpdateFunc)
+	Close() map[string]interface{}
+}
+type UpdateFunc func(interface{}, bool) interface{}
+
+type safeMap chan commandDate
+
+type commandDate struct {
+	action  commandAction
+	key     string
+	value   interface{}
+	result  chan<- interface{}
+	data    chan<- map[string]interface{}
+	updater UpdateFunc
+}
+
+type commandAction int
+
+type findResult struct {
+	value interface{}
+	found bool
+}
+
+func (sm safeMap) Insert(key string, value interface{}) {
+	sm <- commandDate{action: insert, key: key, value: value}
+}
+
+func (sm safeMap) Delete(key string) {
+	sm <- commandDate{action: remove, key: key}
+}
+
+func (sm safeMap) Find(key string) (value interface{}, found bool) {
+	reply := make(chan interface{})
+	sm <- commandDate{action: find, key: key, result: reply}
+	result := (<-reply).(findResult)
+	return result.value, result.found
+}
+
+func (sm safeMap) Len() int {
+	reply := make(chan interface{})
+	sm <- commandDate{action: length, result: reply}
+	return (<-reply).(int)
+}
+
+func (sm safeMap) Update(key string, updater UpdateFunc) {
+	sm <- commandDate{action: update, key: key, updater: updater}
+}
+
+func (sm safeMap) Close() map[string]interface{} {
+	reply := make(chan map[string]interface{})
+	sm <- commandDate{action: end, data: reply}
+	return <-reply
+}
+
+func (sm safeMap) run() {
+	store := make(map[string]interface{})
+	for command := range sm {
+		switch command.action {
+		case insert:
+			store[command.key] = command.value
+		case remove:
+			delete(store, command.key)
+		case find:
+			value, found := store[command.key]
+			command.result <- findResult{value, found}
+		case length:
+			command.result <- len(store)
+		case update:
+			value, found := store[command.key]
+			store[command.key] = command.updater(value, found)
+		case end:
+			close(sm)
+			command.data <- store
+		}
+	}
+}
+
+func NewSafeMap() {
+	sm := make(safeMap)
+	sm.run()
+	return sm
+}
